bed5fd004c905177763c895df72196a368415afe
Emily Georgiades <48098922+EGeorgia@users.noreply.github.com>
2022-12-20 13:03:35 +0000

    Update jobscript-Bulk-ATAC.sh

--- jobscript-Bulk-ATAC.sh
+++ jobscript-Bulk-ATAC.sh
@@ -5,11 +5,10 @@
 #SBATCH --mem=20G
 #SBATCH --output=%j_%x.out
 #SBATCH --error=%j_%x.err
-#SBATCH --mail-user=emily.georgiades@imm.ox.ac.uk
+#SBATCH --mail-user=email@address
 #SBATCH --mail-type=end,fail
 
 
-cd /t1-data/project/fgenomics/egeorgia/Data/Caz-artificial-locus/ATAC/WT-E14/analysis/
-
+cd <path-to-wd>
 bash ./BulkATAC-analysis-pipeline.sh -f sample_name -d ../fastqs -g genome_build -n mm39-AL2R2chrX -p public_directory
 
